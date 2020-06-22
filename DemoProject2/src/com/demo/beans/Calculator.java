11a2174146cb2c5b65d009954a05e6c733a63056
Gireesh <Gireesh@DESKTOP-STCMGSB>
2020-06-22 19:02:43 +0530

    Modify the calculator class by gireesh

--- DemoProject2/src/com/demo/beans/Calculator.java
+++ DemoProject2/src/com/demo/beans/Calculator.java
@@ -5,6 +5,7 @@ public class Calculator {
 	public int add(int a,int b)
 	{
 		System.out.println("This is Add Method in Calculator class");
+		System.out.println("Mater in calculator by gireesh");
 		return a + b;
 	}
 }

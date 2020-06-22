a719dc5c3bc17fbf27b26359f295ae9f56280dca
Gireesh <Gireesh@DESKTOP-STCMGSB>
2020-06-22 16:44:57 +0530

    Changes done on Test.java

--- DemoProject/src/com/demo/test/Test.java
+++ DemoProject/src/com/demo/test/Test.java
@@ -1,11 +1,16 @@
 package com.demo.test;
 
+import java.util.Date;
+
 public class Test {
 
 	public static void main(String[] args) {
           
 		System.out.println("Hi");
 		System.out.println("Hello");
+		Utlity u = new Utlity();
+		Date date = u.getDate();
+		System.out.println(date);
 	}
 
 }
